aa1f38397bf6b27adc252fbf366f15e85194204a
Utkaarsh Saha <62903411+Kishou-Arima@users.noreply.github.com>
2022-11-20 16:24:15 +0530

    test1

---
+++ scripts/player.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+var velocity : Vector2=Vector2()
+var direction : Vector2=Vector2()
+
+func readin():
+	velocity=Vector2()
+	
+	if Input.is_action_pressed("up"):
+		velocity.y -=1
+		direction = Vector2(0,-1)
+		
+	if Input.is_action_pressed("down"):
+		velocity.y +=1
+		direction = Vector2(0,1)
+	if Input.is_action_pressed("left"):
+		velocity.x -=1
+		direction = Vector2(-1,0)
+	if Input.is_action_pressed("right"):
+		velocity.x +=1
+		direction = Vector2(1,0)
+	
+	velocity=velocity.normalized()
+	velocity=move_and_slide(velocity*200)
+
+func _physics_process(delta):
+	readin()
